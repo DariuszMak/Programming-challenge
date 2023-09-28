9bdebb1e1e7bbb4b8f3da303b80ef03a6024a8ab
darMak <darekmakarewiczpraca@gmail.com>
2023-09-28 11:27:58 +0200

    specify file in input

--- src/main.py
+++ src/main.py
@@ -11,7 +11,15 @@ from src.strategies.strategy_a import StrategyCardA
 from src.strategies.strategy_b import StrategyCardB
 
 if __name__ == "__main__":
-    input_file = Path("src", "reference", "problem.dot")
+    # Prompt the user for the input filename
+    input_filename = input("Enter the input filename: ")
+
+    # Create a Path object using the input filename
+    input_file = Path("src", "reference", input_filename)
+
+    if not input_file.exists():
+        print("File not found:", input_filename)
+        sys.exit(1)
 
     try:
         graph = parse_dot_file(input_file)
